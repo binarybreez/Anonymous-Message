5568d983d0d939864df307ea5bc655ddf93a5be8
Rishabh Kushwah <kushwahrishabh159@gmail.com>
2025-01-19 18:52:36 +0530

    connected DB

---
+++ src/lib/connectDB.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+export default async function connnectDB(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to the database using the existing connection.")
+        return
+    }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    console.log("Database connected.");
+    connection.isConnected = db.connections[0].readyState;
+    console.log(db.connections);
+  } catch (error) {
+    console.log("Error connecting the database : ", error);
+    process.exit(1);
+  }
+}
